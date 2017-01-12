6416a20db1dbde40036310495d3c05f770b50cc6
Пользователь <>
2017-01-12 14:28:02 +0500

    v.1.0.3

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,9 @@
-var app = app || {};
-var config = getConfig();
-var template = function(id){
-	return _.template( $('#' + id).html() );
-};
+    var app = app || {};
+    var config = getConfig();
+    var template = function(id){
+        return _.template( $('#' + id).html() );
+    };
 
-$(function() {
-    app.view = getMainView();    
-});
+    $(function() {
+        app.view = getMainView();    
+    });
