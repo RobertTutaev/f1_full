3e646d89d67a96f527b73d17edc8c540ead661c5
Пользователь <>
2017-01-13 15:22:51 +0500

    v.1.0.11

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('css', function () {
 
 //copy js, css, html, ico
 gulp.task('build0', function () {
-    return gulp.src(['[app/*.html', 'app/favicon.ico'], { base: 'app' })
+    return gulp.src(['app/*.html', 'app/favicon.ico'], { base: 'app' })
         .pipe(useref())
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', minifyCss()))
